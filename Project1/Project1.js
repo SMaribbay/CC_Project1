38f9861fbc2fe033872dbd579fbdc16c817e8825
SMaribbay <78175106+SMaribbay@users.noreply.github.com>
2021-03-23 01:30:16 -0400

    Update Project1.js
    Finished swingset model

--- Project1/Project1.js
+++ Project1/Project1.js
@@ -12,4 +12,29 @@ function draw(){
    quad(75,375,190,375,195,380,73,380);
     quad(190,375,195,375,255,475,250,475);
     quad(228,430, 223,430,190,460,195,460);
+  stroke("#8438A2");
+    line(113.33,375, xPos, 440);
+    //frameRate()(20);
+    xPos=xPos+increment;
+    if(xPos<=40){
+      increment=1;
+    }
+      if(xPos>=190){
+        increment=-1;
+      }
+    line(151.66,375, xPos+38.33, 440);
+    xPos=xPos+increment;
+    if(xPos<=70){
+      increment=1;
+    }
+    if(xPos>=220){
+      increment=-1;
+    }
+    quad(xPos,440,xPos+5,445,xPos+38.333,445,xPos+43.333,440);
+    if(xPos<=40){
+      increment=1;
+    }
+    if(xPos>=190){
+      increment=-1;
+    }
 }
